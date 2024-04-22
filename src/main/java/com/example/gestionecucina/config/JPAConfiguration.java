f1af3379d32feb1f43e3c5a8311fff032c8ebed3
giorgio-hash <g.chirico1@studenti.unibg.it>
2024-04-22 15:10:24 +0200

    rimosso file di configurazione non utile
    rimosso JPAConfiguration.java

--- src/main/java/com/example/gestionecucina/config/JPAConfiguration.java
+++
@@ -1,7 +0,0 @@
-package com.example.gestionecucina.config;
-
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-@EnableJpaRepositories(basePackages = "com.example.gestionecucina.Domain.Repository")
-public class JPAConfiguration {
-}
